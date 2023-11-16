0035f47a6318764e44a9b9304a564eacea94b678
Favour Ukonu <favouroukonu@gmail.com>
2023-11-17 00:26:54 +0100

    fixed a bug-tablet,navigation

--- script.js
+++ script.js
@@ -5,10 +5,12 @@
 const btn = document.querySelector("#menu-btn");
 const nav = document.querySelector("#menu");
 
+
 btn.addEventListener("click", () => {
   btn.classList.toggle("open");
   nav.classList.toggle("flex");
   nav.classList.toggle("hidden");
+
 });
 
 // ! POP UP
